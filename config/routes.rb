ab3b51ac330117077ff81c36862c17e8573893a6
Gary <gary.li@berkeley.edu>
2017-11-15 00:03:27 -0800

    added classroom and user models and controllers with basic authentication

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  resources :users
+  resources :sessions
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+end
