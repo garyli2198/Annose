33ecb6c5c08eb1b5544bf07bdefe76c75e918514
kevinliu64 <kevinliu36@berkeley.edu>
2017-11-28 03:35:47 -0800

    fixed dropdown menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,6 +52,6 @@ private
   end
 	
   def allowed_params
- 		params.require(:user).permit(:email, :password, :password_confirmation)
+ 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
