e1cefbb3918f3ee331e9aa1c40d8778483659a64
kevinliu64 <kevinliu36@berkeley.edu>
2017-11-28 00:39:08 -0800

    added user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,7 @@
   		session[:user_id] = user.id
   		redirect_to root_url, notice: 'Logged in!'
 		else
-  		flash.now.alert = 'Email or password is invalid'
-  		render :new
+  		redirect_to login_path, notice: 'Email or password is invalid!'
 		end
 	end
 
